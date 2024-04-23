83d1c7937510ed9b70b4dc5ad92d2bac2c7890d9
Joshua-Frazier <joshua.frazier1519@gmail.com>
2024-04-23 10:35:32 -0600

    added env variable for PORT

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.post("/palette", (req, res) => {
   imageController.handleAPICall(req, res);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
